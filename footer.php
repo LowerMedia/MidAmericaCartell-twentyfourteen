5705c6a08c80568acd347ed84b4815191e7b4d24
hawkeye126 <pete.lower@gmail.com>
2014-05-05 20:31:56 +0000

    add p tags to footer text

--- footer.php
+++ footer.php
@@ -13,11 +13,15 @@
 </div><!-- #page -->
 
 <footer id="colophon" role="contentinfo" class="colophon">
-	<div class='site-copy'>
-		&copy; <?php echo date("Y") ?> MidAmericaCartell
+	<div class='site-copy green'>
+		<p>
+			&copy; <?php echo date("Y") ?> MidAmericaCartell
+		</p>
 	</div>
-	<div class="site-info">
-		Website Designed and Created by <a href="<?php echo esc_url( __( 'http://kelseyhuebner.com/') ); ?>" title="<?php esc_attr_e( 'Tales of car culture from the midwest.' ); ?>">Kelsey Huebner</a> in Association with <a href="<?php echo esc_url( __( 'http://lowermedia.net/') ); ?>" title="<?php esc_attr_e( 'Tales of car culture from the midwest.' ); ?>">LowerMedia</a>
+	<div class="site-info white">
+		<p>
+			Website Designed and Created by <a class="white" href="<?php echo esc_url( __( 'http://kelseyhuebner.com/') ); ?>" title="<?php esc_attr_e( 'Tales of car culture from the midwest.' ); ?>">Kelsey Huebner</a> in Association with <a class="white" href="<?php echo esc_url( __( 'http://lowermedia.net/') ); ?>" title="<?php esc_attr_e( 'Tales of car culture from the midwest.' ); ?>">LowerMedia</a>
+		</p>
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 

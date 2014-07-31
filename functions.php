af4efcc8ab6f908489d8d57877e03d79392067ff
hawkeye126 <pete.lower@gmail.com>
2014-07-31 21:03:14 +0000

    adding some js

--- functions.php
+++ functions.php
@@ -1,1 +1,11 @@
 <?php
+
+function childtheme_scripts() {
+
+//wp_dequeue_script( 'twentytwelve-navigation' );
+
+//wp_enqueue_script( 'primary-mobile', get_stylesheet_directory_uri() . '/navigation.js', array(), '1.0', true );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'childtheme_scripts' );
